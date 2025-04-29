5d94bf820e08bbb5644e221bd32eb33c653475c8
Dobee1Knobee <egorbiriukov231@gmail.com>
2025-04-29 17:11:25 +0300

    erf

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const tg = window.Telegram.WebApp;
+
+const Header = () => {
+    const onClose = () => {
+        tg.close()
+    }
+    return (
+        <div className={'header'}>
+            <button onClick={onClose}>Закрыть</button>
+            <span className={"username"}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
