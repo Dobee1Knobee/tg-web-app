a96e4041b54b3e03e9e427cefa169579f263ff54
Dobee1Knobee <egorbiriukov231@gmail.com>
2025-04-29 20:46:13 +0300

    erf

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
 import "./Header.css"
+import logo from "../../logo/yellowpng.png"
 const Header = () => {
     const {user} = useTelegram();
 
 
     return (
         <div className={'header'}>
+            <img width={"100px"} src={logo} alt={"logo"} style={{marginTop:"2vh",textAlign:"center"}} />
             <span className={"username"}>
                 {user?.username}
             </span>
