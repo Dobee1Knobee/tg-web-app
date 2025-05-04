cc5edb5e3244b194c954c655357f7fada5e7d081
Dobee1Knobee <egorbiriukov231@gmail.com>
2025-05-04 23:08:32 +0300

    erf

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -250,7 +250,6 @@ const Form = () => {
 
             const response = await fetch(url, {
                 method: 'POST',
-                mode: 'no-cors',
                 headers: {
                     'Content-Type': 'application/json',
                 },
